44dec6b820ab90e3d7e54c73e060187fb0ee9d03
Charlie Voiselle <charlie.voiselle@gmail.com>
2018-09-26 13:00:01 -0400

    flex 2

--- shared/scripts/server.sh
+++ shared/scripts/server.sh
@@ -20,9 +20,16 @@ DOCKER_BRIDGE_IP_ADDRESS=(`ifconfig docker0 2>/dev/null|awk '/inet addr:/ {print
 CLOUD=$1
 SERVER_COUNT=$2
 RETRY_JOIN=$3
-NOMAD_BINARY=$4
 
 # Consul
+## Replace existing Consul binary if remote file exists
+if [[ `wget -S --spider $CONSUL_BINARY  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
+  curl -L $CONSUL_BINARY > consul.zip
+  sudo unzip -o consul.zip -d /usr/local/bin
+  sudo chmod 0755 /usr/local/bin/consul
+  sudo chown root:root /usr/local/bin/consul
+fi
+
 sed -i "s/IP_ADDRESS/$IP_ADDRESS/g" $CONFIGDIR/consul.json
 sed -i "s/SERVER_COUNT/$SERVER_COUNT/g" $CONFIGDIR/consul.json
 sed -i "s/RETRY_JOIN/$RETRY_JOIN/g" $CONFIGDIR/consul.json
@@ -35,6 +42,14 @@ export CONSUL_HTTP_ADDR=$IP_ADDRESS:8500
 export CONSUL_RPC_ADDR=$IP_ADDRESS:8400
 
 # Vault
+## Replace existing Vault binary if remote file exists
+if [[ `wget -S --spider $VAULT_BINARY  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
+  curl -L $VAULT_BINARY > vault.zip
+  sudo unzip -o vault.zip -d /usr/local/bin
+  sudo chmod 0755 /usr/local/bin/vault
+  sudo chown root:root /usr/local/bin/vault
+fi
+
 sed -i "s/IP_ADDRESS/$IP_ADDRESS/g" $CONFIGDIR/vault.hcl
 sudo cp $CONFIGDIR/vault.hcl $VAULTCONFIGDIR
 sudo cp $CONFIGDIR/vault.service /etc/systemd/system/vault.service
@@ -60,11 +75,9 @@ sleep 10
 export NOMAD_ADDR=http://$IP_ADDRESS:4646
 
 # Add hostname to /etc/hosts
-
 echo "127.0.0.1 $(hostname)" | sudo tee --append /etc/hosts
 
 # Add Docker bridge network IP to /etc/resolv.conf (at the top)
-
 echo "nameserver $DOCKER_BRIDGE_IP_ADDRESS" | sudo tee /etc/resolv.conf.new
 cat /etc/resolv.conf | sudo tee --append /etc/resolv.conf.new
 sudo mv /etc/resolv.conf.new /etc/resolv.conf
