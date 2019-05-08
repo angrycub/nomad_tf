bb8b7d056d5438669ef8c97221341c65021f9fb2
Lucas de los Santos <lsantos@hashicorp.com>
2019-05-08 08:33:31 -0400

    Replicating chages down to server.sh in client.sh

--- shared/scripts/client.sh
+++ shared/scripts/client.sh
@@ -6,8 +6,6 @@ CONFIGDIR=/ops/shared/config
 
 CONSULCONFIGDIR=/etc/consul.d
 NOMADCONFIGDIR=/etc/nomad.d
-HADOOP_VERSION=hadoop-2.7.6
-HADOOPCONFIGDIR=/usr/local/$HADOOP_VERSION/etc/hadoop
 HOME_DIR=ubuntu
 
 # Wait for network
@@ -18,6 +16,10 @@ IP_ADDRESS="$(/sbin/ifconfig eth0 | awk '/inet / {print $2}')"
 DOCKER_BRIDGE_IP_ADDRESS="$(/sbin/ifconfig docker0 2>/dev/null| awk '/inet / {print $2}')"
 CLOUD=$1
 RETRY_JOIN=$2
+
+# Set env vars for tool CLIs
+echo "export VAULT_ADDR=http://$IP_ADDRESS:8200" | sudo tee --append /home/$HOME_DIR/.bashrc
+echo "export NOMAD_ADDR=http://$IP_ADDRESS:4646" | sudo tee --append /home/$HOME_DIR/.bashrc
 
 # Consul
 sed -i "s/IP_ADDRESS/$IP_ADDRESS/g" $CONFIGDIR/consul_client.json
@@ -33,6 +35,7 @@ if [[ `wget -S --spider $CONSUL_BINARY  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
   sudo chown root:root /usr/local/bin/consul
 fi
 
+sudo systemctl enable consul.service
 sudo systemctl start consul.service
 sleep 10
 
@@ -49,8 +52,8 @@ fi
 sudo cp $CONFIGDIR/nomad_client.hcl $NOMADCONFIGDIR/nomad.hcl
 sudo cp $CONFIGDIR/nomad.service /etc/systemd/system/nomad.service
 
+sudo systemctl enable nomad.service
 sudo systemctl start nomad.service
-export NOMAD_ADDR=http://$IP_ADDRESS:4646
 
 # Add hostname to /etc/hosts
 echo "127.0.0.1 $(hostname)" | sudo tee --append /etc/hosts
@@ -59,21 +62,3 @@ echo "127.0.0.1 $(hostname)" | sudo tee --append /etc/hosts
 echo "nameserver $DOCKER_BRIDGE_IP_ADDRESS" | sudo tee /etc/resolv.conf.new
 cat /etc/resolv.conf | sudo tee --append /etc/resolv.conf.new
 sudo mv /etc/resolv.conf.new /etc/resolv.conf
-
-# Hadoop config file to enable HDFS CLI
-sudo cp $CONFIGDIR/core-site.xml $HADOOPCONFIGDIR
-
-# Move examples directory to $HOME
-sudo mv /ops/examples /home/$HOME_DIR
-sudo chown -R $HOME_DIR:$HOME_DIR /home/$HOME_DIR/examples
-sudo chmod -R 775 /home/$HOME_DIR/examples
-
-# Set env vars for tool CLIs
-echo "export VAULT_ADDR=http://$IP_ADDRESS:8200" | sudo tee --append /home/$HOME_DIR/.bashrc
-echo "export NOMAD_ADDR=http://$IP_ADDRESS:4646" | sudo tee --append /home/$HOME_DIR/.bashrc
-echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre"  | sudo tee --append /home/$HOME_DIR/.bashrc
-
-# Update PATH
-echo "export PATH=$PATH:/usr/local/bin/spark/bin:/usr/local/$HADOOP_VERSION/bin" | sudo tee --append /home/$HOME_DIR/.bashrc
-
-
