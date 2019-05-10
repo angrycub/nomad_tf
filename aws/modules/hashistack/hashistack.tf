0b9a782db554f4ca5fd4dec77197c922eaccd7f7
Lucas de los Santos <anightonhoth@gmail.com>
2019-05-10 09:59:11 -0400

    Removing external ports and forcing the use of SSH tunneling

--- aws/modules/hashistack/hashistack.tf
+++ aws/modules/hashistack/hashistack.tf
@@ -25,46 +25,6 @@ resource "aws_security_group" "primary" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Nomad
-  ingress {
-    from_port   = 4646
-    to_port     = 4646
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Consul
-  ingress {
-    from_port   = 8500
-    to_port     = 8500
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # HDFS NameNode UI
-  ingress {
-    from_port   = 50070
-    to_port     = 50070
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # HDFS DataNode UI
-  ingress {
-    from_port   = 50075
-    to_port     = 50075
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Spark history server UI
-  ingress {
-    from_port   = 18080
-    to_port     = 18080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   ingress {
     from_port = 0
     to_port   = 0
@@ -232,5 +192,3 @@ output "ssh_file" {
     formatlist("Host %v.hs\n  User ubuntu\n  HostName %v\n", aws_instance.client.*.tags.Name, aws_instance.client.*.public_dns)
   ))}"
 }
-
-
