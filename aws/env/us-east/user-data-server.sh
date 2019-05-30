cae726a3df7cdadc51758583de5501819568b943
Lucas de los Santos <anightonhoth@gmail.com>
2019-05-30 13:10:44 -0500

    Adding KMS_KEY and AWS_REGION variables for replacement in vault.hcl as part of post provisioning

--- aws/env/us-east/user-data-server.sh
+++ aws/env/us-east/user-data-server.sh
@@ -4,4 +4,4 @@ set -e
 
 exec > >(sudo tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 sudo chmod +x /ops/shared/scripts/server.sh
-sudo bash -c "NOMAD_BINARY=${nomad_binary} VAULT_BINARY=${vault_binary} CONSUL_BINARY=${consul_binary}  /ops/shared/scripts/server.sh \"aws\" \"${server_count}\" \"${retry_join}\""
+sudo bash -c "KMS_KEY=${kms_key} AWS_REGION=${region} NOMAD_BINARY=${nomad_binary} VAULT_BINARY=${vault_binary} CONSUL_BINARY=${consul_binary}  /ops/shared/scripts/server.sh \"aws\" \"${server_count}\" \"${retry_join}\""
