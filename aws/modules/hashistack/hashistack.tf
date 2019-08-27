c3ce088dcdee50a854c34c9e860e611f1896a95e
Lucas de los Santos <anightonhoth@gmail.com>
2019-08-27 15:41:45 -0400

    removed unmounted/undefined HD from module

--- aws/modules/hashistack/hashistack.tf
+++ aws/modules/hashistack/hashistack.tf
@@ -137,14 +137,6 @@ resource "aws_instance" "client" {
     volume_size = "50"
   }
 
-
-  ebs_block_device = {
-    device_name           = "/dev/xvdd"
-    volume_type           = "gp2"
-    volume_size           = "50"
-    delete_on_termination = "true"
-  }
-
   user_data            = "${data.template_file.user_data_client.rendered}"
   iam_instance_profile = "${aws_iam_instance_profile.instance_profile.name}"
 }
